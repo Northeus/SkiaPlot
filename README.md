638caecc4b87ea09ba9e1eec001699f4dad81e95
Northeus <dark2482000@gmail.com>
2023-06-29 16:31:25 +0200

    feat: feat

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # SkiaPlot
  - Disucuss if this approach is valid at avalonia repo
  - Search for how are usually implemented sizings and layouts
+
+## TODO
+ - Create class that handles printing scales around rectangle
+ - Optimize things that doesnt have to be redrawn unless resized (bitmaps?)
+ - Get some function that would simulate our data
+ - Better controls
+ - Think of the data representation
+ - Create the plot
+ - Add controls
+ - Add point onto image
+ - Numbers bit more dynamic and adjustable
--- playground/Prototype/Plot.cs
+++ playground/Prototype/Plot.cs
@@ -1,18 +1,12 @@
 using System;
-using Avalonia.Utilities;
-using System.Diagnostics;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.LogicalTree;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using Avalonia.Threading;
 using Avalonia.Visuals.Media.Imaging;
-using Avalonia.Visuals.Platform;
-using Avalonia.Platform;
-using System.Runtime.InteropServices;
-using System.Linq;
-using Avalonia.Input;
 
 namespace Prototype;
 
@@ -20,9 +14,33 @@ public class Plot : Control
 {
     private RenderTargetBitmap _bitmap = null!;
 
+    private Bitmap _datamap = null!;
+    private int data_size = 0;
+
+    private int view_x = 0;
+    private int view_y = 0;
+    private int view_size = 0;
+
+    private int box_size = 100;
+
+    private int x_start = 0;
+    private int y_start = 0;
+
+    private int min_tick_x_size = 50;
+    private int min_tick_y_size = 40;
+
+    private int tick_x_count = 2;
+    private int tick_y_count = 5;
+
     protected override void OnAttachedToLogicalTree(LogicalTreeAttachmentEventArgs e)
     {
-        _bitmap = new RenderTargetBitmap(new PixelSize(100, 100), new Vector(96, 96));
+        _bitmap = new RenderTargetBitmap(new PixelSize(box_size, box_size));
+
+        _datamap = new Bitmap("data.bmp");
+        data_size = _datamap.PixelSize.Width == _datamap.PixelSize.Height ? _datamap.PixelSize.Width : -1;
+        view_size = data_size;
+        Console.WriteLine($"Loaded data.bmp with size {data_size}");
+
         base.OnAttachedToLogicalTree(e);
     }
 
@@ -33,31 +51,75 @@ public class Plot : Control
         base.OnDetachedFromLogicalTree(e);
     }
 
-    private int box_size = 100;
-    private int x_start = 0;
-    private int y_start = 0;
+    protected override void OnPointerWheelChanged(PointerWheelEventArgs e)
+    {
+        Console.WriteLine($"X={e.Delta.X} Y={e.Delta.Y}");
 
-    // protected override void OnPointerMoved(PointerEventArgs e)
+        Point pos = e.GetPosition(this) - new Point(x_start, y_start);
+        double x_norm = pos.X / box_size;
+        double y_norm = pos.Y / box_size;
+
+        if (0 <= x_norm && x_norm <= 1 && 0 <= y_norm && y_norm <= 1)
+        {
+            double x_pixel = view_x + x_norm * view_size;
+            double y_pixel = view_y + y_norm * view_size;
+
+            // New view size
+            view_size -= (int) e.Delta.Y * 200;
+            view_size = Math.Max(view_size, 1);
+            view_size = Math.Min(view_size, data_size);
+
+            // Adjust position
+            view_x = (int) (x_pixel - x_norm * view_size);
+            view_y = (int) (y_pixel - y_norm * view_size);
+
+            view_x = Math.Clamp(view_x, 0, data_size - view_size);
+            view_y = Math.Clamp(view_y, 0, data_size - view_size);
+        }
+
+        base.OnPointerWheelChanged(e);
+    }
 
     public override void Render(DrawingContext context)
     {
         if ((_bitmap.Size.Width, _bitmap.Size.Height) != (Bounds.Width, Bounds.Height))
         {
+            // Set up plot size and alignment
             _bitmap = new RenderTargetBitmap(new PixelSize((int)Bounds.Width, (int)Bounds.Height), new Vector(96, 96));
 
             box_size = Math.Max(Math.Min((int)Bounds.Width, (int)Bounds.Height) - 200, 100);
 
             x_start = Math.Max(((int)Bounds.Width - box_size) / 2, 50);
             y_start = Math.Max(((int)Bounds.Height - box_size) / 2, 50);
-        }
 
-        FormattedText text = new("yeey", Typeface.Default, 20.0, TextAlignment.Left, TextWrapping.NoWrap, Size.Infinity);
+            // Set up scales
+            tick_x_count = box_size / min_tick_x_size + 1;
+            tick_y_count = box_size / min_tick_y_size + 1;
+        }
 
         using (var ctx = _bitmap.CreateDrawingContext(null))
         {
-            ctx.Clear(Colors.Aqua);
-            ctx.DrawRectangle(Brushes.Red, null, new Rect(x_start, y_start, box_size, box_size));
-            ctx.DrawText(Brushes.Black, new Point(x_start, y_start + box_size), text.PlatformImpl);
+            ctx.Clear(default);
+            ctx.DrawBitmap(_datamap.PlatformImpl, 1.0, new Rect(view_x, view_y, view_size, view_size), new Rect(x_start, y_start, box_size, box_size), BitmapInterpolationMode.Default);
+            ctx.DrawRectangle(null, new Pen(Brushes.Black), new Rect(x_start, y_start, box_size, box_size));
+
+            FormattedText txt = new("_", Typeface.Default, 14.0, TextAlignment.Left, TextWrapping.NoWrap, Size.Infinity);
+
+            for (int i = 0; i < tick_x_count; i++)
+            {
+                double portion = (double) (i * min_tick_x_size) / box_size;
+
+                txt.Text = portion.ToString("0.000");
+                ctx.DrawText(Brushes.Black, new Point(x_start + i * min_tick_x_size, y_start + box_size), txt.PlatformImpl);
+            }
+
+            for (int i = 0; i < tick_y_count - 1; i++)
+            {
+                double portion = (double) (i * min_tick_y_size) / box_size;
+
+                txt.Text = (1 - portion).ToString("0.0000");
+                ctx.DrawText(Brushes.Black, new Point(x_start - 50, y_start + i * min_tick_y_size), txt.PlatformImpl);
+            }
         }
 
         context.DrawImage(_bitmap, new Rect(0, 0, Bounds.Width, Bounds.Height));
